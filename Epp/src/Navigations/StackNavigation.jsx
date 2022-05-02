9a2f77335798c0b7ff52e927c7cdddf205565e98
Gustavo <gustavodiogo.aluno@unipampa.edu.br>
2022-05-02 14:54:55 -0300

    feat: navigation STACK of screens

--- Epp/src/Navigations/StackNavigation.jsx
+++ Epp/src/Navigations/StackNavigation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { LoginScreen, HomeScreen, PartnerScreen } from '../Screens/'
+import { LoginScreen, HomeScreen, PartnerScreen,EventsScreen } from '../Screens/'
 
 import { ShowBottomTabs } from "./BottomTabsNavigation";
 
@@ -29,6 +29,14 @@ export function PartnersNavigation() {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Partners" component={PartnerScreen} />
+        </Stack.Navigator>
+    )
+}
+
+export function EventsNavigation() {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Events" component={EventsScreen} />
         </Stack.Navigator>
     )
 }
