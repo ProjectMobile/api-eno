406b26369de4be6cab053bae566dc7d801cd8e23
Gustavo <gustavodiogo.aluno@unipampa.edu.br>
2022-05-04 15:36:41 -0300

    feat: event data being storaged in device

--- Epp/src/data/events.jsx
+++ Epp/src/data/events.jsx
@@ -1,9 +1,12 @@
-let events = '';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export function getEvents(){
-    return events;
+
+export async function setEvents(events) {
+    return AsyncStorage.setItem('@events', JSON.stringify(events)).then(json => console.log('success!'))
+        .catch(error => console.log('error!'));
 }
 
-export function setEvents(eventos){
-    events = eventos;
+export async function getEvents() {
+    const eventos = AsyncStorage.getItem('@events')
+    return eventos
 }
