2aff46d38bf174561cec20d3dc4c56441a822788
Gustavo <gustavodiogo.aluno@unipampa.edu.br>
2022-05-04 15:37:01 -0300

    feat: events being showed according to the language selected

--- Epp/src/Screens/events/index.jsx
+++ Epp/src/Screens/events/index.jsx
@@ -1,26 +1,70 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Dimensions, FlatList, StyleSheet, Text, View, Image, Button } from 'react-native';
-import {getEvents} from '../../data/events'
+import { getEvents } from '../../data/events'
+import { getLang } from '../../lang'
 
-function EventsScreen({navigation}) {
+function EventsScreen({ navigation }) {
+
+    const [eventos, setEventos] = useState([])
 
     useEffect(() => {
-            console.log(getEvents())
-    }, [])
+        async function fetchData() {
+            const events = await getEvents();
+            const lang = await getLang();
+            var eventLanguage = []
+            const eventsLLL = JSON.parse(events)
+            eventsLLL.forEach(evento => {
+                if (evento.language === lang) {
+                    eventLanguage.push(evento)
+                }
+            })
+
+            setEventos(eventLanguage)
+
+
+        }
+        fetchData()
+    })
 
     return (
         <View style={styles.container}>
             <Text>EVENTOS</Text>
+            <View style={{ position: 'relative', width: '100%', 'height': '60%', bottom: 20 }}>
+                <FlatList
+                    data={eventos}
+                    renderItem={({ item }) => {
+                        return (
+                            <Text>{item.name}</Text>
+                        )
+                    }}
+                    keyExtractor={eventos => eventos.id}
+                />
+
+            </View>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }, listImcs: {
+        marginTop: 20,
     },
+    resultImcItem: {
+        fontSize: 28,
+        color: "red",
+        height: 50,
+        width: "100%",
+        paddingRight: 20
+    },
+    textResultItemList: {
+        color: "red",
+        fontSize: 16,
+    }
 });
-export {EventsScreen}
+export { EventsScreen }
