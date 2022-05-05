9fdfcf8f247607a46d77d37cff089793ae374554
Gustavo <gustavodiogo.aluno@unipampa.edu.br>
2022-05-05 13:37:48 -0300

    feat: route created for posting infos

--- E-api/src/routes/event.routes.ts
+++ E-api/src/routes/event.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { ListEventController } from '../controller/'
+import { ListEventController, CreateEventController } from '../controller/'
 
 const list = new ListEventController()
-
+const create = new CreateEventController()
 
 const eventRoutes = (router: Router): void => {
-    router.get('/api/event', list.execute.bind(ListEventController))
-  }
-  
-  export { eventRoutes }
+  router.get('/api/event', list.execute.bind(ListEventController))
+  router.post('/api/event', create.execute.bind(CreateEventController))
+}
+
+export { eventRoutes }
