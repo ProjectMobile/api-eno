ef102fb7c909e4224e069ceda74d34f403ea6f49
Gustavo <gustavodiogo.aluno@unipampa.edu.br>
2022-05-27 14:20:57 -0300

    feat: map info being passed as params

--- Epp/src/Screens/home/index.jsx
+++ Epp/src/Screens/home/index.jsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Dimensions, FlatList, StyleSheet, Text, View, Image, Button } from 'react-native';
 import { Maps } from '../../Components/maps/'
-function HomeScreen({ navigation }) {
+function HomeScreen({ navigation, route }) {
     return (
         <View style={styles.container}>
-            <Maps />
+            <Maps
+                latiD={route.params}
+
+            />
         </View>
     );
 }
