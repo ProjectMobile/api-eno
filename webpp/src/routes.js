c7fbb513095caf1d0e33ef070d8dc5dc969d791e
Rafael Ribeiro <rafaelcrd.ribeiro@gmail.com>
2022-06-22 15:57:55 -0300

    back-end comunication working

--- webpp/src/routes.js
+++ webpp/src/routes.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home/index';
-import Login from './Pages/Login/index';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import NewEvent from './Pages/NewEvent';
 
 function Router() {
     return (
@@ -9,6 +10,7 @@ function Router() {
             <Routes>
                 <Route path="/" exact element={<Login />} />
                 <Route path="/Home" exact element={<Home />} />
+                <Route path='/NewEvent' exact element={<NewEvent />} />
             </Routes>
         </BrowserRouter>
     );
