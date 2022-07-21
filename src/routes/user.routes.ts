ba23f5430b60a3881cb9cc48759f9750b1fa5b93
Gustavo Diogo <gustavost1y@gmail.com>
2022-07-20 23:29:01 -0300

    retirando listuser

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express'
-import { listuserController, CreateUserController } from '../controller/'
+import { CreateUserController } from '../controller/'
 
-const list = new listuserController()
 const create = new CreateUserController()
 
 const userRoutes = (router: Router): void => {
-  router.get('/api/user', list.execute.bind(listuserController))
-  router.post('/api/user-admin', create.execute.bind(CreateUserController))
+  // router.post('/api/user-admin', create.execute.bind(CreateUserController))
 }
 
 export { userRoutes }
